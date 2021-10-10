5762cd683abb673354d0bcccef4e6e8236b83a4e
William Abreu <william@t10.digital>
2021-10-10 19:48:40 -0300

    Improved abstract unit_of_work

--- src/core/adapters/unit_of_work.py
+++ src/core/adapters/unit_of_work.py
@@ -1,13 +1,15 @@
-from sqlalchemy import create_engine, orm
-from sqlalchemy.orm import sessionmaker
+from typing import Any, List
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
 
 from src.core.config import get_postgres_uri
 from src.core.ports.unit_of_work import AbstractUnitOfWork
 
-DEFAULT_SESSION_FACTORY = orm.sessionmaker(
+DEFAULT_SESSION_FACTORY = sessionmaker(
+    # ISOLATION LEVEL ENSURES aggregate's version IS RESPECTED
+    # That is, if version differs it will raise an exception
     bind=create_engine(
-        # ISOLATION LEVEL ENSURES aggregate's version IS RESPECTED
-        # That is, if version differs it will raise an exception
         get_postgres_uri(),
         isolation_level="REPEATABLE_READ",
     ),
@@ -16,12 +18,13 @@ DEFAULT_SESSION_FACTORY = orm.sessionmaker(
 
 
 class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
-    session: orm.Session
+    errors: List[Any]
+    session: Session
 
     def __init__(
         self, session_factory: sessionmaker = DEFAULT_SESSION_FACTORY
     ):
-        super().__init__()  # type: ignore
+        self.errors = []
         self.session_factory: sessionmaker = session_factory
 
     def __exit__(self, *args):  # type: ignore
