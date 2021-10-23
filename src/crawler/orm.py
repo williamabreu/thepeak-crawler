e9d4bd4e7d02dc7110cf2990f17c86cb2ee2ea6a
William Abreu <william@t10.digital>
2021-10-23 18:52:06 -0300

    Refactored orm

--- src/crawler/orm.py
+++ src/crawler/orm.py
@@ -1,24 +1,17 @@
-from typing import Callable
+from kingdom_sdk.database.factories import aggregate_table_factory
+from kingdom_sdk.database.mappers import aggregate_mapper
+from sqlalchemy import Column, DateTime, MetaData, String
 
-from sqlalchemy import Column, DateTime, Integer, MetaData, String, Table
-from sqlalchemy.orm import mapper
+from src.crawler.domain import model
 
-import src.crawler.domain.model.track
-from src.crawler.domain import models
-
-tracks: Callable[[MetaData], Table] = lambda metadata: Table(
+tracks = aggregate_table_factory(
     "tracks",
-    metadata,
-    Column(
-        "id", Integer(), primary_key=True, autoincrement=True, nullable=False
-    ),
-    Column("song_time", DateTime(), nullable=False),
-    Column("song_artist", String(128), nullable=False),
-    Column("song_title", String(128), nullable=False),
+    Column("timestamp", DateTime(), nullable=False),
+    Column("artist", String(255), nullable=False),
+    Column("title", String(255), nullable=False),
 )
 
 
-def start_mappers(metadata: MetaData) -> MetaData:
+def start_mappers(metadata: MetaData) -> None:
     tracks_t = tracks(metadata)
-    mapper(src.crawler.domain.model.track.Track, tracks_t)
-    return metadata
+    aggregate_mapper(model.Track, tracks_t)
